fb4acc3a36177389534878e81bab2b4b4d46820d
wjryours <775362296@qq.com>
2019-05-14 20:04:05 +0800

    增加上传部署前端代码

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp')
+const sftp = require('gulp-sftp')
+const fs = require('fs')
+const path = require('path')
+
+const remoteDir = '/root/static/example'
+const remoteStaticDir = '/root/static/example/static'
+const remoteConf = require('./remoteConfig')
+function upload(localPath, conf) {
+    return gulp.src(localPath).pipe(sftp(conf))
+}
+gulp.task('upload',function () {
+    const distDir = path.join(__dirname,'build')
+    const childrenFiles = fs.readdirSync(distDir)
+    const pathConf = []
+    childrenFiles.forEach((file)=>{
+        const fileDir = path.join(distDir,file)
+        const isDirectory = fs.lstatSync(fileDir).isDirectory()
+        if(isDirectory){
+            pathConf.push({
+                localPath:fileDir + '/**',
+                remotePath:remoteStaticDir
+            })
+        }else{
+            pathConf.push({
+                localPath:fileDir,
+                remotePath:remoteDir
+            })
+        }
+    })
+    pathConf.forEach((p)=>{
+        remoteConf.forEach((conf)=>{
+            upload(p.localPath,Object.assign({},conf,{remotePath:p.remotePath}))
+        })
+    })
+})
